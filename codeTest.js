b4d6dede5bacb844a20ab7cec6822019ed0f5675
msmaloo <35118244+msmaloo@users.noreply.github.com>
2020-12-12 01:12:26 -0500

    fixed error

--- codeTest.js
+++ codeTest.js
@@ -3,20 +3,20 @@ function changeFace() {
     if (faceValue < 4) {
         faceValue += 1;
     } 
-    else if (faceValue = 4) {
+    else if (faceValue == 4) {
         faceValue = 1;
     }
 
-    if (faceValue = 1) {
+    if (faceValue == 1) {
         document.getElementById('face').src = "face 1.png";
     }
-    if (faceValue = 2) {
+    if (faceValue == 2) {
         document.getElementById('face').src = "face 2.png";
     }
-    if (faceValue = 3) {
+    if (faceValue == 3) {
         document.getElementById('face').src = "face 3.png";
     }
-    if (faceValue = 4) {
+    if (faceValue == 4) {
         document.getElementById('face').src = "face 4.png";
     }
 
