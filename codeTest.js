10a4b6669d4692e1fc2303157b31593b6ee867ec
msmaloo <35118244+msmaloo@users.noreply.github.com>
2020-12-12 00:53:16 -0500

    added face image cycle
    added logic to cycle through 4 face images

--- codeTest.js
+++ codeTest.js
@@ -1,4 +1,23 @@
-﻿
+﻿var faceValue = 1;
 function changeFace() {
-    document.getElementById('face').src = "face 2.png";
+    if (faceValue < 4) {
+        faceValue += 1;
+    } 
+    else if (faceValue = 4) {
+        faceValue = 1;
+    }
+
+    if (faceValue = 1) {
+        document.getElementById('face').src = "face 1.png";
+    }
+    if (faceValue = 2) {
+        document.getElementById('face').src = "face 2.png";
+    }
+    if (faceValue = 3) {
+        document.getElementById('face').src = "face 3.png";
+    }
+    if (faceValue = 4) {
+        document.getElementById('face').src = "face 4.png";
+    }
+
 }
